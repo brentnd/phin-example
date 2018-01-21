7e2382d02e4ebfd53f6133b1070439aabf98c97a
Brent Dimmig <brentdimmig@gmail.com>
2018-01-21 14:28:35 -0500

    add hero tag and stats sections

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,6 +16,18 @@
         </div>
     </div>
 </section>
+<section class="hero is-info">
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns is-vcentered">
+                <div class="column is-4">
+                    <p class="title">{{ config('site.name') }} Tag</p>
+                    <p class="subtitle">Check this big thing out!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <section id="services" class="section">
     <div class="container has-text-centered">
         <h1 class="title">Services</h1>
@@ -35,6 +47,38 @@
                 <p class="text-muted">{!! $service['content'] !!}</p>
             </div>
             @endforeach
+        </div>
+    </div>
+</section>
+<section class="hero is-danger">
+    <div class="hero-body">
+        <div class="container">
+            <div class="level">
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Tweets</p>
+                        <p class="title">3,456</p>
+                    </div>
+                </div>
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Following</p>
+                        <p class="title">123</p>
+                    </div>
+                </div>
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Followers</p>
+                        <p class="title">456K</p>
+                    </div>
+                </div>
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Likes</p>
+                        <p class="title">789</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
